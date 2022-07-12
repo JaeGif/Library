3a37f68059d26e045ae39afef203b990a8fea111
Jae Gifford <jae.giff@outlook.com>
2022-07-12 10:28:26 +0800

    now adds objects to library correctly based on user input

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ class Library {
         this.books = []
     } addBookToLibrary(bookObj){
         this.books.push(bookObj)
-    } removeBookFromLibrary(bookObj){
+    }/*  removeBookFromLibrary(bookObj){
         this.books.filter()
     } isInLibrary(name) {
-    }
+    } */
 }
 class Book {
     constructor(
@@ -25,27 +25,45 @@ class Book {
 }
 // classes in JS. First instantiate the class as an object, then you can use the methods attached to each class
 const myLibrary = new Library()
-const newBook = new Book('LOTR', 'JRR', 299, false)
 
 /* Open modal on button click */
 const addBookBtn = document.getElementById('add-book')
 const modal = document.getElementById('modal')
 const modalCloseBtn = document.getElementById('close-modal')
 const submitBtn = document.getElementById('submit')
-const userInputName = document.getElementById('name')
+const addBookForm = document.getElementById('form-layout')
 
 addBookBtn.addEventListener('click', () => {
-    modal.style.display = 'flex'
+/*     addBookForm.reset()
+ */    modal.style.display = 'flex'
 })
 modalCloseBtn.addEventListener('click', () => {
     modal.style.display = 'none'
 })
-submitBtn.addEventListener('click', () => {
+         // call add book function when form submits
 
-/*     let newBook = new Book()
- */    modal.style.display = 'none'
-})
+/* submitBtn.addEventListener('click', () => {
+    modal.style.display = 'none'
+}) */
+
+function getBookInfo() {
+    const userInputName = document.getElementById("name").value
+    const userInputAuthor = document.getElementById('author').value
+    const userInputPages = document.getElementById('pages').value
+    const userInputStatus = document.getElementById('status').checked
+    return new Book(userInputName, userInputAuthor,userInputPages,userInputStatus)
+}
+
+const addBookObj = (e) => {
+    e.preventDefault()
+    const newBook = getBookInfo()
+    // if book noot in library, then add to library else =>
+    myLibrary.addBookToLibrary(newBook)
+    console.log(myLibrary)
+}
+
+
+addBookForm.onsubmit = addBookObj
+
+
 /* Random Testing of fns */
-myLibrary.addBookToLibrary(newBook)
-console.log(myLibrary)
-console.log(userInputName.value)
